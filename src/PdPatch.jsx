95cf19de2768ce932bc45821340eb6e0741a5f4c
JoshuaStorm <jsbecker@princeton.edu>
2018-03-31 14:49:33 -0400

    Proof of concept hackiness for setting frequency and moving objects boi

--- src/PdPatch.jsx
+++ src/PdPatch.jsx
@@ -12,6 +12,10 @@ function sanityTestPatch() {
   osc.i(0).message([330])                       // Send frequency of [osc~] to 330Hz
 }
 
+let DAC_INDEX = 0;
+let OSC_INDEX = 1;
+
+
 export default class PdPatch extends Component {
 	constructor(props) {
     super(props);
@@ -19,11 +23,14 @@ export default class PdPatch extends Component {
     this.state = {
       patch: Pd.loadPatch(props.patchString),
       width: null,
-      height: null
+      height: null,
+      oscOn: true
     };
-    
+
     this.props.toggleAudio();
     console.log(this.state.patch);
+    this.toggleOsc = this.toggleOsc.bind(this);
+    this.setFrequency = this.setFrequency.bind(this);
 	}
 
   componentDidMount() {
@@ -34,22 +41,51 @@ export default class PdPatch extends Component {
     });
   }
 
+  // HACKY NONSENSE FOR TESTING
+  toggleOsc() {
+    let outputPort = this.state.patch.objects[OSC_INDEX].o(0);
+    let inputPortL = this.state.patch.objects[DAC_INDEX].i(0);
+    let inputPortR = this.state.patch.objects[DAC_INDEX].i(1);
+
+    if (this.state.oscOn) {
+      this.setState({oscOn: false});
+      this.state.patch.patchData.nodes[OSC_INDEX].layout.x = 420;
+      outputPort.disconnect(inputPortL);
+      outputPort.disconnect(inputPortR);
+    } else {
+      this.setState({oscOn: true});
+      this.state.patch.patchData.nodes[OSC_INDEX].layout.x = 170;
+      outputPort.connect(inputPortL);
+      outputPort.connect(inputPortR);
+    }
+  }
+  // MORE HACKY NONSENSE FOR TESTING
+  setFrequency(event) {
+    let freq = parseFloat(event.target.value);
+    // not sure why not passing value so just flipping cus it's a proof of concept not a feature anwyay
+    this.state.patch.objects[OSC_INDEX].i(0).message([freq]);
+  }
+
+
 	render() {
-    let {patch, width, height} = this.state;
+    let {patch, width, height, oscOn} = this.state;
 
     let {nodes, connections} = patch.patchData;
 
     return (
       <div id="svg-wrapper" ref="wrapper">
+        <button onClick={this.toggleOsc}>{oscOn ? 'oscOff' : 'oscOn'}</button>
+        <input type="range" onChange={(val) => this.setFrequency(val)} step="1" min="0" max="1000" defaultValue="420"></input>
+
         <svg width={width} height={height}>
-          {nodes.map(({id, proto, args, layout, data}) => 
+          {nodes.map(({id, proto, args, layout, data}) =>
             <g key={id}>
               <rect x={layout.x} y={layout.y} height={20} width={50} style={{stroke: '#000', fill: '#fff'}}/>
               <text x={layout.x} y={layout.y}>{proto} {args.join(' ')}</text>
             </g>
           )}
-          {connections.map(({source, sink}, i) => 
-            <path key={i} stroke="black" 
+          {connections.map(({source, sink}, i) =>
+            <path key={i} stroke="black"
               strokeWidth={2}
               d={`M${nodes[source.id].layout.x} ${nodes[source.id].layout.y}
                   L${nodes[sink.id].layout.x} ${nodes[sink.id].layout.y}`}/>
